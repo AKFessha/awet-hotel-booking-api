40b905562ac94cc46acc2011f7346d753fc722f2
Awet Fessha <awetkifle94@gmail.com>
2018-07-10 18:17:56 +0100

    enabled CORS

--- server.js
+++ server.js
@@ -52,6 +52,14 @@ const booking = [
     checkOutDate: "2017-10-02"
   }
 ];
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", function(req, res) {
   res.send(booking);
 });
